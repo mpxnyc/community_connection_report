84df2d1b68079bd226f3f6046d1df1ef7b2c7ab4
Keletso Makofane <keletso@ceb-admin-325.mynetworksettings.com>
2024-05-22 03:45:37 -0400

    working on captions

---
+++ R_functions/_captions_and_titles.R
@@ -0,0 +1,18 @@
+
+captions_and_titles <- function(figure_name){
+  
+    list_captions_and_titles <- list()
+    list_captions_and_titles[["radargroupsex"]]                    <- list()
+    list_captions_and_titles[["radargroupsex"]][["title"]]         <- "Social or sexual contact in group setting over prior 4 weeks"
+    list_captions_and_titles[["radargroupsex"]][["subtitle"]]      <- "MPX NYC, New York, 2022"
+    list_captions_and_titles[["radargroupsex"]][["caption"]]       <- "Data source: MPX NYC 2022"
+    
+    list_captions_and_titles[["places"]]                           <- list()
+    list_captions_and_titles[["places"]][["title"]]                <- "Type of venue for contact over past 4 weeks"
+    list_captions_and_titles[["places"]][["subtitle"]]             <- "MPX NYC, New York, 2022"
+    list_captions_and_titles[["places"]][["caption"]]              <- "Data source: MPX NYC 2022"
+    
+    
+    list_captions_and_titles[[figure_name]]
+}
+
