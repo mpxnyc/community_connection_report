7c0cd4fa978a42ce6d7aaba64694522065d0c863
Keletso Makofane <keletso@ceb-admin-325.mynetworksettings.com>
2024-05-23 08:18:10 -0400

    save

--- R_functions/__analysis_settings.R
+++ R_functions/__analysis_settings.R
@@ -2,8 +2,8 @@ load_initial_settings <- function(){
   
   settings <- list()
   
-  settings[["n_reps_graph"]]     <- 10
-  settings[["n_neighborhoods"]]  <- 50
+  settings[["n_reps_graph"]]     <- 2
+  settings[["n_neighborhoods"]]  <- 67
   settings[["list_intervention_priority"]] <- list("contact", "movement")
   settings[["list_intervention_settings"]] <- list(c("home"), c("home", "social"), c("home", "social", "sexual"))
   
