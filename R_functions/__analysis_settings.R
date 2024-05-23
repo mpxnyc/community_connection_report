bc850990eba660aa5722fb1419a759e5be6ffbf6
Keletso Makofane <keletso@ceb-admin-325.mynetworksettings.com>
2024-05-23 06:06:05 -0400

    almost there

--- R_functions/__analysis_settings.R
+++ R_functions/__analysis_settings.R
@@ -2,7 +2,7 @@ load_initial_settings <- function(){
   
   settings <- list()
   
-  settings[["n_reps_graph"]]     <- 500
+  settings[["n_reps_graph"]]     <- 10
   settings[["n_neighborhoods"]]  <- 50
   settings[["list_intervention_priority"]] <- list("contact", "movement")
   settings[["list_intervention_settings"]] <- list(c("home"), c("home", "social"), c("home", "social", "sexual"))
--- R_functions/_captions_and_titles.R
+++ R_functions/_captions_and_titles.R
@@ -12,17 +12,17 @@ captions_and_titles <- function(figure_name){
     list_captions_and_titles[["radargroupsex"]][["caption"]]             <- "Participants were asked whether they 'had sex with two or more people at the same time, or had close physical contact with multiple people at the same time (e.g. at a party, sport game, concert, show)' over the prior 4 weeks. About two-fifths responded yes."
     
     list_captions_and_titles[["placessexcontact"]]                       <- list()
-    list_captions_and_titles[["placessexcontact"]][["title"]]            <- "Type of venue for contact by type of contact"
+    list_captions_and_titles[["placessexcontact"]][["title"]]            <- "Type of venue for contact vs. type of contact"
     list_captions_and_titles[["placessexcontact"]][["subtitle"]]         <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["placessexcontact"]][["caption"]]          <- "We asked participants to show the location of their home, and the locations of places where they had close physical contact in a group setting. For each place, they indicated what kind of venue it was and whether they had sexual contact there. Among places where participants did not have sex, most were public settings. By contrast, the majority of places for sexual contact were private residences."
     
     list_captions_and_titles[["placesdistance"]]                         <- list()
-    list_captions_and_titles[["placesdistance"]][["title"]]              <- "Type of venue for contact by distance from home"
+    list_captions_and_titles[["placesdistance"]][["title"]]              <- "Type of venue for contact vs. distance from home"
     list_captions_and_titles[["placesdistance"]][["subtitle"]]           <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["placesdistance"]][["caption"]]            <- "We asked participants to show the location of their home, and the locations of places where they had close physical contact in a group setting.  For each place, they indicated what kind of venue it was and whether they had sexual contact there. The further away from home a place was, the less likely it was to be a private residence and the more likely it was to be a dance party."
     
     list_captions_and_titles[["placesdistancesexcontact"]]                <- list()
-    list_captions_and_titles[["placesdistancesexcontact"]][["title"]]     <- "Type of venue for contact by type of contact, distance from home"
+    list_captions_and_titles[["placesdistancesexcontact"]][["title"]]     <- "Type of venue for contact vs. type of contact and distance from home"
     list_captions_and_titles[["placesdistancesexcontact"]][["subtitle"]]  <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["placesdistancesexcontact"]][["caption"]]   <- "We asked participants to show the location of their home, and the locations of places where they had close physical contact in a group setting.  For each place, they indicated what kind of venue it was and whether they had sexual contact there. Though among places where participants had sex, the further the place was, the less likely it was to be a private residence, private residencs were the predominant venue for sexual contact."
     
@@ -37,39 +37,39 @@ captions_and_titles <- function(figure_name){
     list_captions_and_titles[["matrixneighborhood"]][["caption"]]         <- "We asked participants to show the location of their home, and the locations of places where they had close physical contact in a group setting. The axis on the left shows the home community district of each participant, and the top axis shows the destination community. The size of each circle is proportional to the number of respondents with a home in the district on the left who had contact in a district showed at the top."
     
     list_captions_and_titles[["networkcoverage"]]                         <- list()
-    list_captions_and_titles[["networkcoverage"]][["title"]]              <- "Community districts densely connected by study participants"
+    list_captions_and_titles[["networkcoverage"]][["title"]]              <- "Community districts densely connected"
     list_captions_and_titles[["networkcoverage"]][["subtitle"]]           <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["networkcoverage"]][["caption"]]            <- "We asked participants to show the location of their home, and the locations of places where they had close physical contact in a group setting. We constructed a network where nodes are community districts and a tie between a given pair of nodes indicates that study participants were present in both community districts. The thickness of the tie is proportional with the number of study participants who have been present in both community districts."
     
 
     
     list_captions_and_titles[["radarage"]]                 <- list()
-    list_captions_and_titles[["radarage"]][["title"]]      <- "Age at time of participation"
+    list_captions_and_titles[["radarage"]][["title"]]      <- "Age of participants"
     list_captions_and_titles[["radarage"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["radarage"]][["caption"]]    <- ""
     
     list_captions_and_titles[["radarchannel"]]                 <- list()
-    list_captions_and_titles[["radarchannel"]][["title"]]      <- "Study recruitment channel used"
+    list_captions_and_titles[["radarchannel"]][["title"]]      <- "Study recruitment channel"
     list_captions_and_titles[["radarchannel"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["radarchannel"]][["caption"]]    <- ""
     
     list_captions_and_titles[["radargender"]]                 <- list()
-    list_captions_and_titles[["radargender"]][["title"]]      <- "Gender identity at time of participation"
+    list_captions_and_titles[["radargender"]][["title"]]      <- "Gender identity of participants"
     list_captions_and_titles[["radargender"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["radargender"]][["caption"]]    <- ""
     
     list_captions_and_titles[["radarrace"]]                 <- list()
-    list_captions_and_titles[["radarrace"]][["title"]]      <- "Race at time of participation"
+    list_captions_and_titles[["radarrace"]][["title"]]      <- "Race of participants"
     list_captions_and_titles[["radarrace"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["radarrace"]][["caption"]]    <- ""
     
     list_captions_and_titles[["radarsexorientation"]]                 <- list()
-    list_captions_and_titles[["radarsexorientation"]][["title"]]      <- "Sexual orientation at time of participation"
+    list_captions_and_titles[["radarsexorientation"]][["title"]]      <- "Sexual orientation of participants"
     list_captions_and_titles[["radarsexorientation"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["radarsexorientation"]][["caption"]]    <- ""
     
     list_captions_and_titles[["demogroup"]]                 <- list()
-    list_captions_and_titles[["demogroup"]][["title"]]      <- "Demographic characteristics"
+    list_captions_and_titles[["demogroup"]][["title"]]      <- "Demographic characteristics of participants"
     list_captions_and_titles[["demogroup"]][["subtitle"]]   <- "New York, United States,  Jul - Nov 2022"
     list_captions_and_titles[["demogroup"]][["caption"]]    <- ""
     
