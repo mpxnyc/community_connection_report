c62a96875519a0a073a54f9c23ac25f1087f6ef5
“Keletso <keletso.makofane@gmail.com>
2024-05-26 21:08:44 -0400

    standardized figures data flow

--- R_functions/3__make_bipartite_graph.R
+++ R_functions/3__make_bipartite_graph.R
@@ -3,7 +3,8 @@ make_bipartite_graph <- function(place_data, participant_data, analytic_scale = 
   place_edges                 <- place_data %>%
                                     dplyr::rename(from = userId) %>%
                                     dplyr::mutate(to = mpxnyc::convert_spatial_unit_ny(input_census_tract = censusTractPlace, convert_to = analytic_scale)) %>%
-                                    dplyr::select(from, to, names(.))
+                                    dplyr::select(from, to, names(.)) %>%
+                                    dplyr::mutate(home = FALSE)
   
   home_edges                  <- participant_data %>%
                                     dplyr::transmute(
@@ -12,7 +13,8 @@ make_bipartite_graph <- function(place_data, participant_data, analytic_scale = 
                                               intervention_setting = "home", 
                                               placeSex             = "home", 
                                               placeType            = "home"
-                                              ) 
+                                              )  %>%
+                                    dplyr::mutate(home = TRUE)
   
   edges                      <- list(place_edges, home_edges) %>%
                                    dplyr::bind_rows() 
@@ -38,22 +40,3 @@ make_bipartite_graph <- function(place_data, participant_data, analytic_scale = 
   
 }
 
-
-
-if (FALSE) {
-  
-  dirty_data_participants <- readRDS("/Users/keletso/Documents/_data/mpxnyc/mpxnyc_person_data.R")
-  dirty_data_places       <- readRDS("/Users/keletso/Documents/_data/mpxnyc/mpxnyc_place_data.R")
-
-
-  participant_data        <- clean_participant_data(
-                                                  raw_data_participants = dirty_data_participants
-                                                  )
-
-  places_data             <- clean_place_data(
-                                             raw_data_places = dirty_data_places
-                                             )
-                                             
-  make_bipartite_graph(places_data, participant_data)
-}
-  
