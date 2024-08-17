6a0c6a87a10085bde645fdb91ae3865ff822a1ca
“Keletso <keletso.makofane@gmail.com>
2024-08-16 21:56:50 -0400

    fixed initial settings

--- R_functions/__load_initial_settings.R
+++ R_functions/__load_initial_settings.R
@@ -3,10 +3,6 @@ load_initial_settings <- function(){
   settings <- list()
   
   settings[["n_reps_graph"]]     <- 10
-  settings[["n_neighborhoods"]]  <- 67
-  settings[["list_intervention_priority"]] <- list("contact", "movement")
-  settings[["list_intervention_settings"]] <- list(c("home"), c("home", "social"), c("home", "social", "sexual"))
-  
   settings[["analytic_scale"]]  <- "community"
   
   settings
