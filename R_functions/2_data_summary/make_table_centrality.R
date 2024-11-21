cd982fee5ba489ec1df79553d76ffb4f5113ca89
“Keletso <keletso.makofane@gmail.com>
2024-11-21 16:47:46 -0500

    transform to book

--- R_functions/2_data_summary/make_table_centrality.R
+++ R_functions/2_data_summary/make_table_centrality.R
@@ -1,7 +1,7 @@
 make_table_centrality <- function(){
-  tar_read(data_intervention_results_centrality_collected) %>%
-    filter(intervention_stratification == "overall") %>%
-    filter(intervention_ranking <= 40) %>%
-    mutate(intervention_priority = factor(intervention_priority, c("contact", "movement"), c("Contact-based strategy", "Movement-based strategy"))) %>%
-    mutate(group = factor(group, c("n_vaccinated", "n_remainder", "n_lcc"), c("Vaccinated", "Unvaccinated in LCC", "Unvaccinated not in LCC")))
+  targets::tar_read(data_intervention_results_centrality_collected) |>
+    dplyr::filter(intervention_stratification == "overall") |>
+    dplyr::filter(intervention_ranking <= 40) |>
+    dplyr::mutate(intervention_priority = factor(intervention_priority, c("contact", "movement"), c("Contact-based strategy", "Movement-based strategy"))) |>
+    dplyr::mutate(group = factor(group, c("n_vaccinated", "n_remainder", "n_lcc"), c("Vaccinated", "Unvaccinated in LCC", "Unvaccinated not in LCC")))
 }
