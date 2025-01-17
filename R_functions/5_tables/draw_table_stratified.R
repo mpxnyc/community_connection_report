8ff0133f9e685476388ebf4b8951c46482213d24
“Keletso <keletso.makofane@gmail.com>
2025-01-17 02:28:06 -0500

    commmit

--- R_functions/5_tables/draw_table_stratified.R
+++ R_functions/5_tables/draw_table_stratified.R
@@ -1,22 +1,22 @@
 draw_table_stratified <- function(table){
-  table %>%
-    select(stratum, level, count, proportion, proportion_ci_lb, proportion_ci_ub) %>%
-    mutate(level = str_replace(level, "\n", " ")) %>%
-    mutate(
+  table |>
+    dplyr::select(stratum, level, count, proportion, proportion_ci_lb, proportion_ci_ub) |>
+    dplyr::mutate(level = stringr::str_replace(level, "\n", " ")) |>
+    dplyr::mutate(
       proportion = scales::percent(proportion, accuracy = 2, suffix = ""),
       proportion_ci_lb = scales::percent(proportion_ci_lb, accuracy = 2, suffix = ""),
       proportion_ci_ub = scales::percent(proportion_ci_ub, accuracy = 2, suffix = "")
-    ) %>%
-    transmute(stratum, Group = level, N = count, `% (95% CI)` = paste0(proportion, " (", proportion_ci_lb," to " , proportion_ci_ub, ")")) %>%
-    gt::gt(groupname_col = "stratum") %>%
+    ) |>
+    dplyr::transmute(stratum, Group = level, N = count, `% (95% CI)` = paste0(proportion, " (", proportion_ci_lb," to " , proportion_ci_ub, ")")) |>
+    gt::gt(groupname_col = "stratum") |>
     gt::cols_align(
       align = "left",
       columns = Group
-    ) %>%
+    ) |>
     gt::cols_align(
       align = "center",
       columns = `% (95% CI)`
-    ) %>%
+    ) |>
     gt::cols_align(
       align = "center",
       columns = N
--- R_functions/5_tables/draw_table_stratified2.R
+++ R_functions/5_tables/draw_table_stratified2.R
@@ -1,23 +1,23 @@
 draw_table_stratified2 <- function(table){
-  table %>%
-    ungroup() %>%
-    select(stratum_a, stratum_b, level, count, proportion, proportion_ci_lb, proportion_ci_ub) %>%
-    mutate(level = str_replace(level, "\n", " ")) %>%
-    mutate(
+  table |>
+    dplyr::ungroup() |>
+    dplyr::select(stratum_a, stratum_b, level, count, proportion, proportion_ci_lb, proportion_ci_ub) |>
+    dplyr:: mutate(level = stringr::str_replace(level, "\n", " ")) |>
+    dplyr::mutate(
       proportion = scales::percent(proportion, accuracy = 2, suffix = ""),
       proportion_ci_lb = scales::percent(proportion_ci_lb, accuracy = 2, suffix = ""),
       proportion_ci_ub = scales::percent(proportion_ci_ub, accuracy = 2, suffix = "")
-    ) %>%
-    transmute(stratum_a, stratum_b, Group = level, N = count, `% (95% CI)` = paste0(proportion, " (", proportion_ci_lb," to " , proportion_ci_ub, ")")) %>%
-    gt::gt(groupname_col = c("stratum_a", "stratum_b") ) %>%
+    ) |>
+    dplyr::transmute(stratum_a, stratum_b, Group = level, N = count, `% (95% CI)` = paste0(proportion, " (", proportion_ci_lb," to " , proportion_ci_ub, ")")) |>
+    gt::gt(groupname_col = c("stratum_a", "stratum_b") ) |>
     gt::cols_align(
       align = "left",
       columns = Group
-    ) %>%
+    ) |>
     gt::cols_align(
       align = "center",
       columns = `% (95% CI)`
-    ) %>%
+    ) |>
     gt::cols_align(
       align = "center",
       columns = N
