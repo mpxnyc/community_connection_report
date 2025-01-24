4b01e7e880f7216c4e625ac97cec50839d09f888
“Keletso <keletso.makofane@gmail.com>
2025-01-23 20:21:25 -0500

    commit

--- R_functions/3_plot/plot_icon.R
+++ R_functions/3_plot/plot_icon.R
@@ -1,11 +1,11 @@
-plot_icon                 <- function(icon_name = "unicorn", color = "light_purple", shape = 3, alpha = 1){
+plot_icon                 <- function(icon_name = "unicorn", color = "light_purple", shape = 3, alpha = 1, size = 80){
   
   mpxnyc_colors <- targets::tar_read(mpxnyc_colors)
   
   
   ggplot2::ggplot() + 
     ggplot2::theme_void() +
-    ggplot2::geom_point(ggplot2::aes(x = 1, y = 1), size = 80, color =  mpxnyc_colors[[color]], shape = shape, alpha = alpha) +
+    ggplot2::geom_point(ggplot2::aes(x = 1, y = 1), size = size, color =  mpxnyc_colors[[color]], shape = shape, alpha = alpha) +
     ggimage::geom_image(ggplot2::aes(image=targets::tar_read(mpxnyc_emojis)[[icon_name]], x = 1, y = 1), size = 0.4) 
   
   
