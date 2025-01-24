d702d233da13f18595ef9b91ff6728042f8274fa
“Keletso <keletso.makofane@gmail.com>
2025-01-24 03:08:32 -0500

    commit

--- R_functions/__load_emojis.R
+++ R_functions/__load_emojis.R
@@ -5,17 +5,17 @@ load_emojis <- function(root = here::here()){
   
   emojis <- list()
   
-  emojis[["bear"]]        <- paste0(root, "/__const/emojis/MPX_bear.svg")
-  emojis[["devil"]]       <- paste0(root, "/__const/emojis/MPX_devil.svg")
-  emojis[["droplets"]]    <- paste0(root, "/__const/emojis/MPX_droplets.svg")
+  emojis[["bear"]]        <- paste0(root, "/const/emojis/MPX_bear.svg")
+  emojis[["devil"]]       <- paste0(root, "/const/emojis/MPX_devil.svg")
+  emojis[["droplets"]]    <- paste0(root, "/const/emojis/MPX_droplets.svg")
   
-  emojis[["eggplant"]]    <- paste0(root, "/__const/emojis/MPX_eggplant.svg")
-  emojis[["peach"]]       <- paste0(root, "/__const/emojis/MPX_peach.svg")
-  emojis[["pignose"]]     <- paste0(root, "/__const/emojis/MPX_pignose.svg")
+  emojis[["eggplant"]]    <- paste0(root, "/const/emojis/MPX_eggplant.svg")
+  emojis[["peach"]]       <- paste0(root, "/const/emojis/MPX_peach.svg")
+  emojis[["pignose"]]     <- paste0(root, "/const/emojis/MPX_pignose.svg")
   
-  emojis[["taco"]]        <- paste0(root, "/__const/emojis/MPX_taco.svg")
-  emojis[["tulip"]]       <- paste0(root, "/__const/emojis/MPX_tulip.svg")
-  emojis[["unicorn"]]     <- paste0(root, "/__const/emojis/MPX_unicorn.svg")
+  emojis[["taco"]]        <- paste0(root, "/const/emojis/MPX_taco.svg")
+  emojis[["tulip"]]       <- paste0(root, "/const/emojis/MPX_tulip.svg")
+  emojis[["unicorn"]]     <- paste0(root, "/const/emojis/MPX_unicorn.svg")
 
   
   emojis
--- R_functions/__load_images.R
+++ R_functions/__load_images.R
@@ -4,10 +4,10 @@ load_images <- function(root = here::here()){
   
   images <- list()
   
-  images[["app_graphic"]]                  <- paste0(root, "/__const/images/image_app_graphic.png")
-  images[["campaign_graphic"]]             <- paste0(root, "/__const/images/image_campaign_graphic.png") 
-  images[["creative_requirements"]]        <- paste0(root, "/__const/images/image_creative_requirements.png")  
-  images[["mpxny"]]                        <- paste0(root, "/__const/images/image_mpxny.png")  
+  images[["app_graphic"]]                  <- paste0(root, "/const/images/image_app_graphic.png")
+  images[["campaign_graphic"]]             <- paste0(root, "/const/images/image_campaign_graphic.png") 
+  images[["creative_requirements"]]        <- paste0(root, "/const/images/image_creative_requirements.png")  
+  images[["mpxny"]]                        <- paste0(root, "/const/images/image_mpxny.png")  
 
 
   
