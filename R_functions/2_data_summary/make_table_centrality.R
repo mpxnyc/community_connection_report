ef1d64ce17ab7cec0b52c525c3779207adba282f
“Keletso <keletso.makofane@gmail.com>
2025-05-02 03:42:09 -0400

    fixed diagrams

--- R_functions/2_data_summary/make_table_centrality.R
+++ R_functions/2_data_summary/make_table_centrality.R
@@ -2,6 +2,6 @@ make_table_centrality <- function(){
   targets::tar_read(data_intervention_results_centrality_collected) |>
     dplyr::filter(intervention_stratification == "overall") |>
     dplyr::filter(intervention_ranking <= 40) |>
-    dplyr::mutate(intervention_priority = factor(intervention_priority, c("contact", "movement"), c("Contact-based strategy", "Movement-based strategy"))) |>
+    dplyr::mutate(intervention_priority = factor(intervention_priority, c("contact", "movement"), c("Contact\nneutralizing\napproach", "Movement\nneutralizing\napproach"))) |>
     dplyr::mutate(group = factor(group, c("n_vaccinated", "n_remainder", "n_lcc"), c("Vaccinated", "Unvaccinated in LCC", "Unvaccinated not in LCC")))
 }
