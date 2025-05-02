bc61d64757fcc04ecda0975cd1c946c541c5795e
“Keletso <keletso.makofane@gmail.com>
2025-05-02 02:51:55 -0400

    protect

--- R_functions/3_plot/plot_home_vs_residence_dbn_bar.R
+++ R_functions/3_plot/plot_home_vs_residence_dbn_bar.R
@@ -4,9 +4,9 @@ plot_home_vs_residence_dbn_bar <- function(data){
     ggplot2::geom_bar(
       ggplot2::aes(x = community, y = weight, fill = borough, alpha = highlight), 
       stat = "identity", 
-      size = 0.6, 
-      width = 0.8,
-      color = "black",
+      size = 0.2, 
+      width = 1,
+      color = "white",
       position = "dodge"
     ) 
   
