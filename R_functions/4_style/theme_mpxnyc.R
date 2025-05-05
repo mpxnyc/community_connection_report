5f130578dab27cb93f3e84a9a4e25bc8b7732313
“Keletso <keletso.makofane@gmail.com>
2025-05-05 01:08:35 -0400

    cleaning code

--- R_functions/4_style/theme_mpxnyc.R
+++ R_functions/4_style/theme_mpxnyc.R
@@ -9,6 +9,7 @@ theme_mpxnyc_blank <- function(...){
       legend.text            = ggplot2::element_text(size = 12),
       legend.margin          = ggplot2::margin(40,10,10,10),
       plot.margin            = ggplot2::unit(c(1.5, 1.5, 1.5, 1.5), "cm"),
+      plot.title             = ggplot2::element_text(hjust = 0.5),
       strip.background       = ggplot2::element_blank(),
       strip.text             = ggplot2::element_text(size = 13, margin = ggplot2::margin(40,40,40,40)),
       strip.text.y.left      = ggplot2::element_text(angle = 0),
