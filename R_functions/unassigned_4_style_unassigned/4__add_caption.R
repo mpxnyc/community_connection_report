d6a2f945e8839c28c2bfbde90b23b6a52b6af974
“Keletso <keletso.makofane@gmail.com>
2025-05-15 05:50:07 -0400

    all in

--- R_functions/unassigned_4_style_unassigned/4__add_caption.R
+++
@@ -1,7 +0,0 @@
-add_caption <- function(plot, caption = "Data source: MPX NYC Survey (restricted to 1,304 residents of New York City). www.mpxresponse.org", wrap_n = 5000000){
-  
-  plot + 
-    ggplot2::labs(
-      caption  = caption |> stringr::str_wrap(wrap_n)  
-    )
-}
