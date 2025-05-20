8d89afecb8753946677ce5ff438f2ed952459065
“Keletso <keletso.makofane@gmail.com>
2025-05-20 18:33:27 -0400

    before refactor

--- R_functions/5_style_main_report/4__theme_mpxnyc_radar_people.R
+++ R_functions/5_style_main_report/4__theme_mpxnyc_radar_people.R
@@ -2,7 +2,7 @@ theme_mpxnyc_radar_people <- function(...){
   theme_mpxnyc_blank() +
     ggplot2::theme(
       legend.position = "bottom",
-      plot.margin = ggplot2::unit(c(3,2,3,2), "cm")
+      plot.margin = ggplot2::unit(c(0,2,3,2), "cm")
     ) +
     ggplot2::theme(...)
 }
