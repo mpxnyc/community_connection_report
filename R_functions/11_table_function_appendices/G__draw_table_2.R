c788876aa09693b7776eae5ad35fee57daa2823a
“Keletso <keletso.makofane@gmail.com>
2025-05-21 04:16:00 -0400

    fixed

--- R_functions/11_table_function_appendices/G__draw_table_2.R
+++ R_functions/11_table_function_appendices/G__draw_table_2.R
@@ -4,6 +4,17 @@
 draw_table_2 <- function(data, label){
   
   data |>
+    dplyr::mutate(
+      proportion       = scales::percent(proportion, 1),
+      proportion_ci_lb = scales::percent(proportion_ci_lb, 1, suffix = ""),
+      proportion_ci_ub = scales::percent(proportion_ci_ub, 1, suffix = "")
+    ) |>
+    dplyr::transmute(
+      stratum,
+      group            = level,
+      count            = count, 
+      percentage       = paste0(proportion, " (", proportion_ci_lb, "-", proportion_ci_ub, ")")
+    ) |>
     gt::gt(groupname_col = "stratum") |>
     gt::tab_header(
       title = label,
--- R_functions/11_table_function_appendices/G__draw_table_3.R
+++ R_functions/11_table_function_appendices/G__draw_table_3.R
@@ -3,6 +3,18 @@
 draw_table_3 <- function(data, label){
   
   data |>
+    dplyr::mutate(
+      proportion       = scales::percent(proportion, 1),
+      proportion_ci_lb = scales::percent(proportion_ci_lb, 1, suffix = ""),
+      proportion_ci_ub = scales::percent(proportion_ci_ub, 1, suffix = "")
+    ) |>
+    dplyr::transmute(
+      stratum_a,
+      stratum_b,
+      group            = level,
+      count            = count, 
+      percentage       = paste0(proportion, " (", proportion_ci_lb, "-", proportion_ci_ub, ")")
+    ) |>
     gt::gt(groupname_col = c("stratum_a", "stratum_b")) |>
     gt::tab_header(
       title = label,
