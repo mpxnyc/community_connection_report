0bd320232ea6c02fa0112d848ccf7a18f053cb68
“Keletso <keletso.makofane@gmail.com>
2025-05-21 04:16:00 -0400

    fixed

--- R_functions/11_table_function_appendices/G__draw_table_1.R
+++ R_functions/11_table_function_appendices/G__draw_table_1.R
@@ -3,6 +3,17 @@
 draw_table_1 <- function(data, label){
   
   data |>
+    dplyr::select(level, count, proportion, proportion_ci_lb, proportion_ci_ub) |>
+    dplyr::mutate(
+      proportion       = scales::percent(proportion, 1),
+      proportion_ci_lb = scales::percent(proportion_ci_lb, 1, suffix = ""),
+      proportion_ci_ub = scales::percent(proportion_ci_ub, 1, suffix = "")
+    ) |>
+    dplyr::transmute(
+      group            = level,
+      count            = count, 
+      percentage       = paste0(proportion, " (", proportion_ci_lb, "-", proportion_ci_ub, ")")
+    ) |>
     gt::gt() |>
     gt::tab_header(
       title = label,
