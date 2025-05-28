145084f8b73589fe2fc1979074ea286dec81196d
“Keletso <keletso.makofane@gmail.com>
2025-05-27 23:57:19 -0400

    adding maps

---
+++ R_functions/6_table_data_main_report/6__make_tabledata_residence.R
@@ -0,0 +1,9 @@
+make_table_data_residence <- function(){
+  targets::tar_read(data_bipartite_graph_collected) |>
+    tidygraph::activate(nodes)|>
+    data.frame() |>
+    dplyr::filter(type) |>
+    dplyr::group_by(borough, community, neighborhood) |>
+    dplyr::summarize(count = dplyr::n()) |>
+    dplyr::arrange(-count)
+}
