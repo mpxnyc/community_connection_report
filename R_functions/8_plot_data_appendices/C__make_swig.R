af622b4ea6e238ddd39caab093f1c7d7cf980bde
“Keletso <keletso.makofane@gmail.com>
2025-06-16 18:34:29 -0400

    commit

--- R_functions/8_plot_data_appendices/C__make_swig.R
+++ R_functions/8_plot_data_appendices/C__make_swig.R
@@ -37,6 +37,13 @@ make_swig <- function(graph_type){
     graph_nodes_list <- append(graph_nodes_list, list(data.frame(name = "Y3", label = r"($Y_3^{(a_3, a_{N_3})}$)"        , x = 5, y = 5,   homogenous = FALSE, node_shape = "covariate")))
   }
   
+  if (graph_type == "full_interference"){
+    graph_nodes_list <- append(graph_nodes_list, list(data.frame(name = "Y1", label = r"($Y_1^{(a_1, a_2, a_3)}$)"  , x = 5, y = 1,   homogenous = FALSE, node_shape = "covariate")))
+    graph_nodes_list <- append(graph_nodes_list, list(data.frame(name = "Y2", label = r"($Y_2^{(a_1, a_2, a_3)}$)"   , x = 5, y = 3,   homogenous = FALSE, node_shape = "covariate")))
+    graph_nodes_list <- append(graph_nodes_list, list(data.frame(name = "Y3", label = r"($Y_3^{(a_1, a_2, a_3)}$)"         , x = 5, y = 5,   homogenous = FALSE, node_shape = "covariate")))
+  }
+  
+  
   
   graph_nodes <- graph_nodes_list |>
     dplyr::bind_rows() |>
@@ -68,10 +75,9 @@ make_swig <- function(graph_type){
     rbind(graph_edges_list[["network_interference"]] )
   
   graph_edges_list[["fully_connected"]] <- data.frame(
-    from                 = c("L1", "a1", "Y1"),
-    to                   = c("L2", "A2", "Y2")
-  ) |>
-    rbind(graph_edges_list[["full_interference"]] )
+    from                 = c("L1", "a1", "Y1", "L1", "a1", "Y1", "L2", "A2", "Y2"),
+    to                   = c("L2", "A2", "Y2", "L3", "A3", "Y3", "L3", "A3", "Y3")
+  )
   
   
   
