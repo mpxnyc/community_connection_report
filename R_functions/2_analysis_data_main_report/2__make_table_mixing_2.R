3e6d29329876ec0f58554ec09beac61e9a6c6a54
“Keletso <keletso.makofane@gmail.com>
2025-10-27 19:25:22 -0400

    update

--- R_functions/2_analysis_data_main_report/2__make_table_mixing_2.R
+++ R_functions/2_analysis_data_main_report/2__make_table_mixing_2.R
@@ -1,40 +1,88 @@
 make_table_mixing_2       <- function(variable){
   
+  helper_breakdown_into_graph_list <- function(current_rep){
+
+    
+    working_graph_simulation |> 
+      tidygraph::filter(rep == current_rep) |>
+      igraph::bipartite.projection(which = "true") |>
+      tidygraph::as_tbl_graph(directed = FALSE)
+    
+  }
   
-  working_graph                <- targets::tar_read(data_bipartite_graph_simulated) |>
-    igraph::bipartite.projection(which = "true") |>
-    tidygraph::as_tbl_graph(directed = FALSE) 
+  helper_get_specific_degree_data <- function(current_graph){
+
+    
+    current_graph |>
+      dplyr::mutate(ego_level = {{variable}}) |>
+      specific_degree({{variable}}) |>
+      dplyr::mutate(preference = specific_degree / degree) |>
+      data.frame()
+  }
   
+  helper_get_preference_data <- function(current_working_graph, current_specific_degree_data){
+
+    
+    current_working_graph |>
+      data.frame() |>
+      dplyr::left_join(current_specific_degree_data, by = c("name", "rep")) |>
+      dplyr::mutate(ego_level = {{variable}}) |>
+      dplyr::group_by(rep, ego_level, alter_level) |>
+      dplyr::summarize(preference = mean(preference, na.rm = TRUE)) |>
+      dplyr::ungroup()
+    
+  }
   
-  specific_degree_data         <-  working_graph |>
-    dplyr::mutate(ego_level = {{variable}}) |>
-    specific_degree({{variable}}) |>
-    dplyr::mutate(preference = specific_degree / degree) |>
-    data.frame()
+  helper_get_prevalence_data <- function(current_graph){
+
+    
+    current_graph |>
+      data.frame() |>
+      dplyr::mutate(level = {{variable}}) |>
+      dplyr::group_by(rep, level) |>
+      dplyr::summarize(count = dplyr::n()) |>
+      dplyr::ungroup() |>
+      dplyr::group_by(rep) |>
+      dplyr::mutate(prevalence = count / sum(count)) |>
+      dplyr::ungroup()
+  }
   
-  preference                    <- working_graph |>
-    data.frame() |>
-    dplyr::left_join(specific_degree_data, by = c("name", "rep")) |>
-    dplyr::mutate(ego_level = {{variable}}) |>
-    dplyr::group_by(rep, ego_level, alter_level) |>
-    dplyr::summarize(preference = mean(preference, na.rm = TRUE)) |>
-    dplyr::ungroup()
+  helper_get_selection_coef_data <- function(preference_data, prevalence_data){
+
+    
+    preference_data |>
+      dplyr::left_join(prevalence_data, by = c("rep", "alter_level" = "level")) |>
+      dplyr::mutate(selection_coef = preference / prevalence ) |>
+      dplyr::select(rep, ego_level, alter_level, selection_coef)
+  }
   
-  prevalence                    <- working_graph |>
-    data.frame() |>
-    dplyr::mutate(level = {{variable}}) |>
-    dplyr::group_by(rep, level) |>
-    dplyr::summarize(count = dplyr::n()) |>
-    dplyr::ungroup() |>
-    dplyr::group_by(rep) |>
-    dplyr::mutate(prevalence = count / sum(count)) |>
-    dplyr::ungroup()
+  working_graph_simulation                <- targets::tar_read(data_bipartite_graph_simulated) 
+    
+  reps_vector                             <- working_graph_simulation |>
+                                                tidygraph::activate(nodes) |>
+                                                tidygraph::pull(rep) |>
+                                                unique()
   
+  working_graph_list                      <- reps_vector |>
+                                                purrr::map(helper_breakdown_into_graph_list) 
   
-  preference |>
-    dplyr::left_join(prevalence, by = c("rep", "alter_level" = "level")) |>
-    dplyr::mutate(selection_coef = preference / prevalence ) |>
-    dplyr::select(rep, ego_level, alter_level, selection_coef) |>
+  specific_degree_data_list               <-  working_graph_list |>
+                                                purrr::map(helper_get_specific_degree_data)
+     
+
+  preference_data_list                    <- working_graph_list |> 
+                                                purrr::map2(specific_degree_data_list, helper_get_preference_data)
+  
+
+  prevalence_data_list                    <- working_graph_list |>
+                                                purrr::map(helper_get_prevalence_data)
+    
+  
+  selection_coef_data_list                <- preference_data_list |> 
+                                                purrr::map2(prevalence_data_list, helper_get_selection_coef_data)
+  
+  selection_coef_data_list |>
+    dplyr::bind_rows() |>
     dplyr::group_by(ego_level, alter_level) |>
     dplyr::summarize(
       mean_selection_coef = mean(selection_coef), 
