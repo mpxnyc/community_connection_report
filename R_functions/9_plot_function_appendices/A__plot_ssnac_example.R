d54feef8de2d40d0094477b11c2e5b084b3ba99f
“Keletso <keletso.makofane@gmail.com>
2025-11-10 20:51:58 -0500

    save

--- R_functions/9_plot_function_appendices/A__plot_ssnac_example.R
+++ R_functions/9_plot_function_appendices/A__plot_ssnac_example.R
@@ -9,7 +9,7 @@ plot_ssnac_example <- function(data){
   )
   
   ggraph::ggraph(example_snacc_graph, coords) +
-    ggraph::geom_edge_link(ggplot2::aes(edge_linetype = edge_type, width = edge_width)) +
-    ggraph::geom_node_circle(ggplot2::aes(r = 0.1, color = node_color, filter = node_type == "Person"), fill = "white") + 
-    ggraph::geom_node_circle(ggplot2::aes(r = 0.1, color = node_color, filter = node_type == "Place"), fill = "grey")
+    ggraph::geom_edge_link2(ggplot2::aes(edge_linetype = edge_line_type, width = edge_line_width)) +
+    ggraph::geom_node_circle(ggplot2::aes(r = 0.1, color = node_type, filter = node_type == "Person"), fill = "white") + 
+    ggraph::geom_node_circle(ggplot2::aes(r = 0.1, color = node_type, filter = node_type == "Place"), fill = "grey")
 }
