c6f0a9fc168239dc68b7313696ba3f04bfa26f61
“Keletso <keletso.makofane@gmail.com>
2025-12-29 22:26:44 -0500

    commit

--- R_functions/5_style_main_report/4__theme_mpxnyc_mixing.R
+++ R_functions/5_style_main_report/4__theme_mpxnyc_mixing.R
@@ -1,8 +1,8 @@
 theme_mpxnyc_mixing <- function(...){
   theme_mpxnyc_blank() +
     ggplot2::theme(
-      axis.text.y              = ggplot2::element_text(size = 15,  vjust = 0.5, hjust = 0.5, color = "#636363"),
-      axis.text.x              = ggplot2::element_text(angle = -90, size = 15, hjust = 0.5, vjust = 0.5, color = "#636363"),
+      axis.text.y              = ggplot2::element_text(size = 15,  vjust = 0.5, hjust = 1, color = "#636363"),
+      axis.text.x              = ggplot2::element_text(angle = -90, size = 15, hjust = 0, vjust = 0.5, color = "#636363"),
       legend.position          = "bottom",
       legend.title             = ggplot2::element_text( margin = ggplot2::margin(30, 30, 30, 30), hjust = 1),
       legend.text              = ggplot2::element_text(size = 10),
