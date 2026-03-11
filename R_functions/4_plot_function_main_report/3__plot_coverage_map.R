dad42609c648dc4cf020dccd07ebfe516f7d87c0
“Keletso <keletso.makofane@gmail.com>
2026-03-11 17:03:12 -0400

    annotate maps

--- R_functions/4_plot_function_main_report/3__plot_coverage_map.R
+++ R_functions/4_plot_function_main_report/3__plot_coverage_map.R
@@ -1,6 +1,6 @@
 plot_coverage_map        <- function(data){
   
-  data |>
+  working_data <- data |>
     dplyr::pull(stratum) |>
     unique() |>
     purrr::map(
@@ -13,10 +13,28 @@ plot_coverage_map        <- function(data){
           dplyr::mutate(stratum = x)
       }
     ) |>
-    dplyr::bind_rows() |>
+    dplyr::bind_rows()
+  
+
+  working_data |>
     ggplot2::ggplot() +
     ggplot2::geom_sf(fill = "grey", color = "white") +
     ggplot2::geom_sf(ggplot2::aes(fill = group), color = "white", linewidth = 0.5) + 
-    ggplot2::facet_wrap(stratum ~ ., strip.position = "left", ncol = 1)
+    ggplot2::facet_wrap(stratum ~ ., strip.position = "left", ncol = 1) +
+    ggrepel::geom_label_repel(
+      ggplot2::aes(label = name, geometry = geometry, fill = group),
+      stat = "sf_coordinates",
+      data = subset(working_data, group == "Group A"),
+      min.segment.length = 0, # Always draw lines
+      size = 6,
+      force = 100,             # Force labels away from each other
+      direction = "y",
+      alpha = 0.7,
+      nudge_x = -25000,
+      nudge_y = 25000,   # Nudge labels to the right,
+      linecolor = "grey",
+      linewidth = 2,
+      show.legend = FALSE
+    ) 
 
 }
